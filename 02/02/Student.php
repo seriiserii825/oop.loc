ae03f776b6bbac617e1febb21bafc58eb3dfee33
serii <seriiburduja@gmail.com>
2020-02-29 08:53:46 +0200

    02:51

--- 02/02/Student.php
+++ 02/02/Student.php
@@ -16,29 +16,85 @@ class Student
 
 	public function getFullName()
 	{
-		return $this->lastName . ' ' .$this->firstName;
+		return $this->lastName . ' ' . $this->firstName;
 	}
+
 }
 
-function loadStudentsFromTxt($file)
+class TxtStudentReposytory
 {
-	$rows = file($file);
-	$students = [];
+	private $file;
 
-	foreach ($rows as $row) {
-		$values = array_map('trim', explode(';', $row));
-		$students[] = new Student($values[0], $values[1], $values[2]);
+	public function __construct($file)
+	{
+		$this->file = $file;
 	}
 
-	return $students;
+	public function findAll()
+	{
+		$rows = file($this->file);
+		$students = [];
+		foreach ($rows as $row) {
+			$values = array_map('trim', explode(';', $row));
+			$students[] = new Student($values[0], $values[1], $values[2]);
+		}
+		return $students;
+	}
+
+	public function saveAll(array $students)
+	{
+		$rows = [];
+		foreach ($students as $student) {
+			$rows[] = implode(';', [
+				$student->lastName,
+				$student->firstName,
+				$student->birthDate
+			]);
+		}
+		file_put_contents($this->file, implode(PHP_EOL, $rows));
+	}
 }
 
-$file = __DIR__ . '/list.txt';
+class CsvStudentRepository {
+	private $file;
 
-$students = loadStudentsFromTxt($file);
+	public function __construct($file)
+	{
+		$this->file = $file;
+	}
+
+	public function findAll()
+	{
+		$rows = file($this->file);
+		$students = [];
+		foreach ($rows as $row) {
+			$values = array_map('trim', explode(',', $row));
+			$students[] = new Student($values[0], $values[1], $values[2]);
+		}
+		return $students;
+	}
+}
+
+
+//#########################A
+$type = 'txt';
+$file = __DIR__.'/list.txt';
+//$file = __DIR__.'/students.csv';
+
+switch ($type){
+	case "txt":
+		$studentRepository = new TxtStudentReposytory($file);
+		break;
+
+	case "csv":
+		$studentRepository = new CsvStudentRepository($file);
+		break;
+}
+//#########################A
+
+$students = $studentRepository->findAll();
 
 foreach ($students as $student) {
 	echo $student->getFullName() . ' ' . $student->birthDate . PHP_EOL;
 }
-
 
