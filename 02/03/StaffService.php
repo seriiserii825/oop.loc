b2383ca848b9d6ab56cb3e31fe61a39a695bd645
serii <seriiburduja@gmail.com>
2020-03-28 07:32:14 +0200

    lessong 03

--- 02/03/StaffService.php
+++ 02/03/StaffService.php
@@ -5,38 +5,42 @@ class Name
 	private $first;
 	private $last;
 
-	public function __construct($first, $last) {
+	public function __construct($first, $last)
+	{
 		$this->first = $first;
 		$this->last = $last;
 	}
 
-	public function getFirst() {
+	public function getFirst()
+	{
 		return $this->first;
 	}
 
-	public function getLast() {
+	public function getLast()
+	{
 		return $this->last;
 	}
 
 	public function getFullName()
 	{
-		return $this->first.' '.$this->last;
+		return $this->first . ' ' . $this->last;
 	}
 
 	/**
 	 * @param mixed $first
 	 */
-	public function setFirst($first) {
+	public function setFirst($first)
+	{
 		$this->first = $first;
 	}
 
 	/**
 	 * @param mixed $last
 	 */
-	public function setLast($last) {
+	public function setLast($last)
+	{
 		$this->last = $last;
 	}
-
 }
 
 
@@ -45,36 +49,46 @@ class Phone
 	private $code;
 	private $number;
 
-	public function __construct($code, $number) {
+	public function __construct($code, $number)
+	{
 		$this->code = $code;
 		$this->number = $number;
+	}
+
+	public function is_equal($phone)
+	{
+		return $this->code == $phone->code && $phone->number === $this->number;
 	}
 
 	/**
 	 * @return mixed
 	 */
-	public function getCode() {
+	public function getCode()
+	{
 		return $this->code;
 	}
 
 	/**
 	 * @return mixed
 	 */
-	public function getNumber() {
+	public function getNumber()
+	{
 		return $this->number;
 	}
 
 	/**
 	 * @param mixed $code
 	 */
-	public function setCode(Phone $code) {
+	public function setCode(Phone $code)
+	{
 		$this->code = $code;
 	}
 
 	/**
 	 * @param mixed $number
 	 */
-	public function setNumber(Phone $number) {
+	public function setNumber(Phone $number)
+	{
 		$this->number = $number;
 	}
 }
@@ -85,7 +99,8 @@ class Address
 	private $city;
 	private $street;
 
-	public function __construct($country, $city, $street) {
+	public function __construct($country, $city, $street)
+	{
 		$this->country = $country;
 		$this->city = $city;
 		$this->street = $street;
@@ -94,46 +109,65 @@ class Address
 	/**
 	 * @return mixed
 	 */
-	public function getCity() {
+	public function getCity()
+	{
 		return $this->city;
 	}
 
 	/**
 	 * @return mixed
 	 */
-	public function getStreet() {
+	public function getStreet()
+	{
 		return $this->street;
 	}
 
 	/**
 	 * @return mixed
 	 */
-	public function getCountry() { return $this->country; }
+	public function getCountry()
+	{
+		return $this->country;
+	}
 }
 
 class Employee
 {
 	public $id;
 	public $name;
-	public $phone;
+	public $phones = [];
 	public $address;
 
-	public function __construct($id, Name $name, Phone $phone, Address $address) {
+	public function __construct($id, Name $name, array $phones, Address $address)
+	{
 		$this->id = $id;
 		$this->name = $name;
-		$this->phone = $phone;
+		$this->phone = $phones;
 		$this->address = $address;
 	}
 
-	public function getId() { return $this->id; }
-	public function getName() { return $this->name->getFullName(); }
-	public function getPhone() { return $this->phone; }
-	public function getAddress() { return $this->address; }
+	public function getId()
+	{
+		return $this->id;
+	}
+	public function getName()
+	{
+		return $this->name->getFullName();
+	}
+	public function getPhone()
+	{
+		return $this->phones;
+	}
+	public function getAddress()
+	{
+		return $this->address;
+	}
 
 	/**
 	 * @param Name $name
 	 */
-	public function rename($firstName, $lastName) {
+	public function rename($firstName, $lastName)
+	{
 		$this->name->setFirst($firstName);
 		$this->name->setLast($lastName);
 	}
@@ -141,30 +175,52 @@ class Employee
 	/**
 	 * @param Phone $phone
 	 */
-	public function changePhone($phone) {
-		$this->phone = $phone;
+	public function addPhone(Phone $phone)
+	{
+		foreach ($this->phones as $item) {
+			if ($item->is_equal($phone)) {
+				throw new \Exception('Phone already exist');
+			}
+		}
+		$this->phones[] = $phone;
+	}
+	public function deletePhone(Phone $phone)
+	{
+		foreach($this->phones as $i => $item){
+			if($item->is_equal($phone)){
+				unset($this->phones[$i]);
+				return;
+			}
+		}
+		throw new \Exception('Phone not found.');
 	}
 
 	/**
 	 * @param Address $address
 	 */
-	public function changeAddress($address) {
+	public function changeAddress($address)
+	{
 		$this->address = $address;
 	}
 }
 
 class StaffService
 {
-	public function recruiteEmployee(Name $name, Phone $phone, Address $address) {
-		$employee = new Employee($this->generateId(), $name, $phone, $address);
+	public function recruiteEmployee(Name $name, Phone $phone, Address $address)
+	{
+		$employee = new Employee($this->generateId(), $name, [$phone], $address);
 		$this->save($employee);
 		return $employee;
 	}
 
-	public function generateId() { return rand(1000, 9999); }
+	public function generateId()
+	{
+		return rand(1000, 9999);
+	}
 
-	public function save($employee) {
-		$file = __DIR__ . '/data/employee_' . $employee->address->getCountry() . '_' . $employee->phone->getNumber() . '.php';
+	public function save($employee)
+	{
+		$file = __DIR__ . '/data/employee_' . $employee->address->getCountry() . '.php';
 		$data = var_export($employee, true);
 		file_put_contents($file, $data);
 	}
@@ -173,8 +229,19 @@ class StaffService
 $service = new StaffService();
 $employee = $service->recruiteEmployee(new Name('Vasya', 'Pupkin'), new Phone('373', '444444'), new Address('Molova', 'Cahul', 'Miroshnichenko 14'));
 
-echo $employee->getName() . PHP_EOL;
+$employee->addPhone(new Phone('7', '220798'));
+$employee->addPhone(new Phone('373', '060562168'));
 
-$employee->rename('Vova', 'Putin');
+foreach($employee->getPhone() as $item){
+	var_dump($item);
+}
+echo "After".PHP_EOL;
 
-echo $employee->getName() . PHP_EOL;
+$employee->deletePhone(new Phone('7', '220798'));
+
+foreach($employee->getPhone() as $item){
+	var_dump($item);
+}
+// echo $employee->getName() . PHP_EOL;
+// $employee->rename('Vova', 'Putin');
+// echo $employee->getName() . PHP_EOL;
---
+++ 02/03/data/employee_Molova.php
@@ -0,0 +1,25 @@
+Employee::__set_state(array(
+   'id' => 9621,
+   'name' => 
+  Name::__set_state(array(
+     'first' => 'Vasya',
+     'last' => 'Pupkin',
+  )),
+   'phones' => 
+  array (
+  ),
+   'address' => 
+  Address::__set_state(array(
+     'country' => 'Molova',
+     'city' => 'Cahul',
+     'street' => 'Miroshnichenko 14',
+  )),
+   'phone' => 
+  array (
+    0 => 
+    Phone::__set_state(array(
+       'code' => '373',
+       'number' => '444444',
+    )),
+  ),
+))
